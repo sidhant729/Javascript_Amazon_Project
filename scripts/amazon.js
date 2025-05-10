bf0385e99c4da15de1f4e06d8ffa50b6ad729786
sidhant729 <iamsidhantjha729@gmail.com>
2025-05-10 18:07:45 +0530

    Added Cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,37 +1,7 @@
-const product = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        star: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-      rating: {
-          star: 4.0,
-          count: 127
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-      rating: {
-          star: 4.5,
-          count: 56
-    },
-    priceCents: 799,
-  },
-];
-
 addProducts = (product) => {
   let content = "";
   product.forEach((element) => {
-      content += `   
+    content += `   
         <div class="product-container"> 
         <div class="product-image-container">
             <img class="product-image"
@@ -44,7 +14,7 @@ addProducts = (product) => {
 
         <div class="product-rating-container">
             <img class="product-rating-stars"
-            src="/images/ratings/rating-${element.rating.star * 10}.png">
+            src="/images/ratings/rating-${element.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
             ${element.rating.count}
             </div>
@@ -84,4 +54,4 @@ addProducts = (product) => {
   });
   document.querySelector(".js-products-grid").innerHTML = content;
 };
-addProducts(product);
+addProducts(products);
